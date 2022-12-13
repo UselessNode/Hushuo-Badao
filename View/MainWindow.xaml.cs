7066cc93ff2b443bf3d867d07e7726978032fc24
Koko <78096367+UselessNode@users.noreply.github.com>
2022-12-13 09:23:24 +0700

    Вывод таблицы INCOICES

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppDB.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace AppDB
         public MainWindow()
         {
             InitializeComponent();
+            InvoicesDataGrid.ItemsSource = new SupplementEntities().INVOICES.ToList();
         }
     }
 }
