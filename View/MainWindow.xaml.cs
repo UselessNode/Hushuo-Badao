ed38bb29791b2567ac8527c36ba1847221c68513
Koko <78096367+UselessNode@users.noreply.github.com>
2022-12-17 02:55:36 +0700

    Поиск и фильтрация + Визуальные правки

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -41,6 +41,7 @@ namespace AppDB
         {
             Entities = new SupplementEntities();
             InvoicesDataGrid.ItemsSource = Entities.INVOICES.ToList();
+            Title = $"База данных";
         }
 
         public void UpdateRecordButton_Click(object sender, RoutedEventArgs e)
@@ -60,6 +61,54 @@ namespace AppDB
                 Entities.SaveChanges();
                 ReadData();
             }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var input = (sender as TextBox).Text.ToLower();
+            if (!(String.IsNullOrEmpty(input)))
+            {
+                int resultCount = Entities.INVOICES.Count(x => x.PRODUCTS.PRODUCT_NAME.Contains(input));
+                InvoicesDataGrid.ItemsSource = Entities.INVOICES.Where(x => x.PRODUCTS.PRODUCT_NAME.Contains(input)).ToList();
+                Title = $"База данных | Поиск: {input} | Результатов: {resultCount} из {Entities.INVOICES.ToList().Count()}";
+            }
+            else
+                ReadData();
+        }
+
+        private void ResetSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchTextBox.Text = "";
+            DatePickerEnd.Text = DatePickerStart.Text = "";
+            ReadData();
+        }
+
+        private void DatePickerEnd_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DatePickerEnd.SelectedDate != null && DatePickerStart.SelectedDate != null)
+            {
+                if(DatePickerEnd.SelectedDate > DatePickerStart.SelectedDate)
+                {
+                    var start = DatePickerStart.SelectedDate;
+                    var end = DatePickerEnd.SelectedDate;
+                    //MessageBox.Show($"END: {DatePickerEnd.SelectedDate} \n START: {DatePickerStart.SelectedDate}","ДА",
+                    //    MessageBoxButton.OK,MessageBoxImage.Information);
+
+                    var filteredData = Entities.INVOICES.Where(x => x.DATE_OF_INVOICE > start && x.DATE_OF_INVOICE < end).ToList();
+
+                    InvoicesDataGrid.ItemsSource = filteredData;
+                }
+                else
+                {
+                    //MessageBox.Show($"END: {DatePickerEnd.SelectedDate} \n START: {DatePickerStart.SelectedDate}", "НЕТ",
+                    //    MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void DatePickerStart_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
